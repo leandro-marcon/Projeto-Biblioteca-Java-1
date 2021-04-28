756d1b32c357cec0786051867de3db82e8e955e0
William Heleodoro <williamheleodoro@gmail.com>
2021-04-27 21:42:05 -0300

    Adicionado o HomeController

---
+++ src/unoesc/edu/Biblioteca/controller/HomeController.java
@@ -0,0 +1,19 @@
+package unoesc.edu.Biblioteca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+	@Controller
+	public class HomeController {
+		
+		@RequestMapping(path = "/", method = RequestMethod.GET)
+		public String acesso() {
+			System.out.println("Acessou");
+			
+			return "index";
+		}
+
+	}
+
+
